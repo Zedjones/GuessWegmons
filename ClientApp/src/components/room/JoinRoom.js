bc42288851e43200a7dc34262be1e0d48fb92c22
coryo12345 <corydio12@gmail.com>
2020-02-08 16:11:01 -0500

    error messages for rooms

--- ClientApp/src/components/room/JoinRoom.js
+++ ClientApp/src/components/room/JoinRoom.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import { Input } from 'reactstrap';
+import { Alert } from 'reactstrap';
 
 export class JoinRoom extends Component {
     constructor(props) {
         super(props)
-        this.state = {textval: ''}
+        this.state = {textval: '', showAlert: false}
         // function binding
         this.joinWithCode = this.joinWithCode.bind(this)
         this.handleChange = this.handleChange.bind(this)
@@ -16,16 +17,29 @@ export class JoinRoom extends Component {
     }
 
     joinWithCode() {
-        // TODO call api to validate and join room
-        this.props.joinRoom(this.state.textval)
+        fetch(`/api/room/join?id=${this.state.textval}`, {method: 'post'})
+        .then((resp) => {
+            if(!resp.ok)
+                throw Error("That room doesn't exist")
+            return resp
+        })
+        .then((resp) => {
+            this.props.joinRoom(this.state.textval) 
+            this.setState({showAlert: false})
+        })
+        .catch(error => {
+            this.setState({codeErr: error.message, showAlert: true})
+        })
     }
 
     render() {
+        const alertStyle = (this.state.showAlert) ? {display: 'block'} : {display: 'none'}
         return (
             <div>
                 <h2>Or Join A Room</h2>
                 <Input type="text" value={this.state.textval} style={codeInputStyle} onChange={this.handleChange} className="center" />
                 <Button onClick={this.joinWithCode}>Join Room</Button>
+                <Alert style={alertStyle} color="warning">{this.state.codeErr}</Alert>
             </div>
         )
     }
@@ -33,4 +47,4 @@ export class JoinRoom extends Component {
 
 const codeInputStyle = {
     maxWidth: '10em'
-};
+}
--- ClientApp/src/components/room/NewRoom.js
+++ ClientApp/src/components/room/NewRoom.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
+import { Alert } from 'reactstrap';
 
 export class NewRoom extends Component {
     constructor(props) {
         super(props)
-
+        this.state = {showAlert: false}
         // function binding
         this.getCode = this.getCode.bind(this)
     }
 
     getCode() {
-        // TODO call api to get new room code
-        var code = 0 // fetch(...)
-        this.props.joinRoom(code)
+        fetch('/api/room/create', {method: 'post'})
+        .then((resp) => {
+            if (!resp.ok)
+                throw Error("")
+            return resp.json()
+        })
+        .then((resp) => {
+            this.props.joinRoom(resp.id)
+            this.setState({showAlert: false})
+        })
+        .catch((error) => {
+            this.setState({codeErr: "Something went wrong. Try again later.", showAlert: true})
+        })
     }
 
     render() {
+        const alertStyle = (this.state.showAlert) ? {display: 'block'} : {display: 'none'}
         return (
             <div>
+                <Alert style={alertStyle} color="info">{this.state.codeErr}</Alert>
                 <h2>Start A New Room</h2>
                 <Button onClick={this.getCode}>Create</Button>
             </div>
