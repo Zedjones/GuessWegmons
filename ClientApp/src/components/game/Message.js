85b547cc23866cf067088ce87be848e22e32d670
Ellie Parobek <ellie.parobek@gmail.com>
2020-02-08 20:11:31 -0500

    History/Message

---
+++ ClientApp/src/components/game/Message.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+function Message(props){
+    return (
+        <div>
+            <h4>{props.player} asks: {props.question}</h4>
+            <h4>{props.answer}</h4>
+        </div>
+    )
+}
