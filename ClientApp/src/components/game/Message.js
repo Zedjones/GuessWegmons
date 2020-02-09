11528c7f6e39d4bccb2959e49fb02be506f491ef
coryo12345 <corydio12@gmail.com>
2020-02-08 20:19:41 -0500

    fixign imports

--- ClientApp/src/components/game/Message.js
+++ ClientApp/src/components/game/Message.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
-function Message(props){
-    return (
-        <div>
-            <h4>{props.player} asks: {props.question}</h4>
-            <h4>{props.answer}</h4>
-        </div>
-    )
+export class History extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>{props.player} asks: {props.question}</h4>
+                <h4>{props.answer}</h4>
+            </div>
+        )
+    }
 }
