d78f727eb0581e556a2fcecfd830938099d66e97
coryo12345 <corydio12@gmail.com>
2020-02-08 19:44:37 -0500

    gamecards v1

--- ClientApp/src/components/game/History.js
+++ ClientApp/src/components/game/History.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 
 export class History extends Component {
+    constructor(props) {
+        super(props)
+    }
 
+    render() {
+        return (
+            <div></div>
+        )
+    }
 }
