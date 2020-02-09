cf82d226b1af32689409f8c56a57702b3aea22c0
coryo12345 <corydio12@gmail.com>
2020-02-08 20:03:17 -0500

    gamecards working

--- ClientApp/src/components/game/Board.js
+++ ClientApp/src/components/game/Board.js
@@ -4,7 +4,7 @@ import { GameCard } from './GameCard';
 export class Board extends Component {
     constructor(props) {
         super(props)
-        this.state = { boardInfo: null }
+        this.state = { board: null }
     }
 
     componentDidMount() {
@@ -19,14 +19,34 @@ export class Board extends Component {
     }
 
     render() {
-        if (this.state.boardInfo) {
+        if (this.state.board) {
             return (
-                <div>
+                <div style={boardStyle}>
                     <GameCard img={this.state.board[0].pictureURL} name={this.state.board[0].name}></GameCard>
                     <GameCard img={this.state.board[1].pictureURL} name={this.state.board[1].name}></GameCard>
                     <GameCard img={this.state.board[2].pictureURL} name={this.state.board[2].name}></GameCard>
                     <GameCard img={this.state.board[3].pictureURL} name={this.state.board[3].name}></GameCard>
                     <GameCard img={this.state.board[4].pictureURL} name={this.state.board[4].name}></GameCard>
+                    <GameCard img={this.state.board[5].pictureURL} name={this.state.board[5].name}></GameCard>
+                    <GameCard img={this.state.board[6].pictureURL} name={this.state.board[6].name}></GameCard>
+                    <GameCard img={this.state.board[7].pictureURL} name={this.state.board[7].name}></GameCard>
+                    <GameCard img={this.state.board[8].pictureURL} name={this.state.board[8].name}></GameCard>
+                    <GameCard img={this.state.board[9].pictureURL} name={this.state.board[9].name}></GameCard>
+                    <GameCard img={this.state.board[10].pictureURL} name={this.state.board[10].name}></GameCard>
+                    <GameCard img={this.state.board[11].pictureURL} name={this.state.board[11].name}></GameCard>
+                    <GameCard img={this.state.board[12].pictureURL} name={this.state.board[12].name}></GameCard>
+                    <GameCard img={this.state.board[13].pictureURL} name={this.state.board[13].name}></GameCard>
+                    <GameCard img={this.state.board[14].pictureURL} name={this.state.board[14].name}></GameCard>
+                    <GameCard img={this.state.board[15].pictureURL} name={this.state.board[15].name}></GameCard>
+                    <GameCard img={this.state.board[16].pictureURL} name={this.state.board[16].name}></GameCard>
+                    <GameCard img={this.state.board[17].pictureURL} name={this.state.board[17].name}></GameCard>
+                    <GameCard img={this.state.board[18].pictureURL} name={this.state.board[18].name}></GameCard>
+                    <GameCard img={this.state.board[19].pictureURL} name={this.state.board[19].name}></GameCard>
+                    <GameCard img={this.state.board[20].pictureURL} name={this.state.board[20].name}></GameCard>
+                    <GameCard img={this.state.board[21].pictureURL} name={this.state.board[21].name}></GameCard>
+                    <GameCard img={this.state.board[22].pictureURL} name={this.state.board[22].name}></GameCard>
+                    <GameCard img={this.state.board[23].pictureURL} name={this.state.board[23].name}></GameCard>
+                    <GameCard img={this.state.board[24].pictureURL} name={this.state.board[24].name}></GameCard>
                 </div>
             )
         } else {
@@ -37,4 +57,9 @@ export class Board extends Component {
             )
         }
     }
+}
+
+const boardStyle = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr'
 }
--- ClientApp/src/components/game/GameCard.js
+++ ClientApp/src/components/game/GameCard.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 export class GameCard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
+        this.state = {}
     }
 
-    render(){
+    render() {
         return (
             <div>
-
+                <Card style={cardStyle}>
+                    <CardImg top width="100%" src={this.props.img} alt={this.props.name} />
+                    <CardBody>
+                        <CardTitle style={titleStyle}>{this.props.name}</CardTitle>
+                        {/* <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button</Button> */}
+                    </CardBody>
+                </Card>
             </div>
         )
     }
 
+}
+
+const cardStyle = {
+    margin: '5px'
+}
+
+const titleStyle = {
+    textAlign: 'center'
 }
