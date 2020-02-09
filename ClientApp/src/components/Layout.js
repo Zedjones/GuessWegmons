737eb80d7742dd16b9487f3a56a919bd5fc5a84b
Lowell Pence <lxp3901@rit.edu>
2020-02-08 19:44:37 -0500

    nav bar 😎

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
+import { NavMenu } from './NavMenu'
 
 export class Layout extends Component {
     static displayName = Layout.name;
@@ -7,7 +8,7 @@ export class Layout extends Component {
     render() {
         return (
             <div>
-                {/* <NavMenu /> */}
+                <NavMenu />
                 <Container style={topStyle}>
                     {this.props.children}
                 </Container>
